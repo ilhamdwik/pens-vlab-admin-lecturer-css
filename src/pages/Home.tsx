f3b8b32716f60e9c0c1ba706988b68190ec2f94f
ilhamdwik <ilhaskam@gmail.com>
2022-06-30 16:55:28 +0700

    commit terakhir

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Card } from "../components/Card";
 import { RootState } from "../redux/store";
+import "../assets/styles/home.css";
+// import { Card } from "../components/Card";
 // import nProgress from "nprogress";
 // import { getStudents } from "../redux/actions/studentsActions";
 // import { students } from "../types";
@@ -31,14 +32,21 @@ const Home = () => {
   
   // console.log(data)
   return (
-    <div className="p-6">
-      <div className="grid grid-cols-1 gap-6">
-        <Card className="bg-gradient-to-br dark:from-blue-900 dark:to-blue-800 from-blue-200 to-blue-100 shadow-none">
-          <div>
-            <div>Hallo, how are you ?</div>
-            <div className="text-xl font-bold mt-px">{user?.name}</div>
-          </div>
-        </Card>
+    // <div className="p-6">
+    //   <div className="grid grid-cols-1 gap-6">
+
+    //     <Card className="bg-gradient-to-br dark:from-blue-900 dark:to-blue-800 from-blue-200 to-blue-100 shadow-none">
+    //       <div>
+    //         <div>Hallo, how are you ?</div>
+    //         <div className="text-xl font-bold mt-px">{user?.name}</div>
+    //       </div>
+    //     </Card>
+    //   </div>
+    // </div>
+    
+    <div className="typing-animation">
+      <div className="text-box">
+          <h2>Hello <span className="user-name">{user?.name}</span></h2>
       </div>
     </div>
   );
